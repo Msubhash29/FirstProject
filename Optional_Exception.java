98ca52c39c45c6584e1658386fe08f38a3f9c00d
Msubhash29 <126698826+Msubhash29@users.noreply.github.com>
2023-03-01 23:11:17 +0530

    Create Optional_Exception

---
+++ Optional_Exception.java
@@ -0,0 +1,67 @@
+package javatask5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.*
+;public class Optional_Exception {
+	
+		   
+	private static String name;
+	
+
+   public void setName(String name) {
+	   this.name=name;
+   }
+
+	public String getName() {
+		return name;
+	}
+
+	
+
+	public double getMark() {
+		return mark;
+	}
+
+	
+
+	private double mark;
+
+	public  Optional_Exception(String name, double mark) {
+		this.name=name;
+		this.mark=mark;
+	}
+	
+	public static void main(String[] args) {
+   		
+		
+		
+  
+	 /*Optional<Optional_Exception> getResult()
+			{ 
+		  
+		 
+				List<Optional_Exception> result = new ArrayList<>();
+				result.add(new Optional_Exception("Subhash", 80));
+				result.add(new Optional_Exception("Chandra", 75));
+				result.add(new Optional_Exception("Bose",50));
+				result.add(new Optional_Exception("Seeni", 100));
+				result.add(new Optional_Exception("Parsanth",80));
+				
+				return;
+			}*/
+		
+	  
+		List<Optional_Exception> result = new ArrayList<>();
+		Scanner sc=new Scanner(System.in);
+		String name="Subhash";
+       Optional<String> oe=setName(name);
+       System.out.println(oe);
+      
+       
+	}		
+}
+
+
